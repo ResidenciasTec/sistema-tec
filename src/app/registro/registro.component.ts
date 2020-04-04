978c4dad5698fc9d84f33418d6a7fe33a66654f2
JesusEMH <47198161+JesusEMH@users.noreply.github.com>
2020-04-03 19:31:39 -0500

    formularios registro y login  actualizados

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,69 +1,77 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../models/user';
 import {UserService} from "../services/user.service";
-import {departamentoService} from "../services/departamento.service";
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss'],
-  providers: [UserService, departamentoService]
+  providers: [UserService]
 })
 export class RegistroComponent implements OnInit {
   public title:string;
-  public user:User;
   public status: string;
-  public departamentos: string;
+  public loading: boolean;
+	public form: FormGroup;
 
   constructor(
   	private _userService: UserService,
-    private _departamentoService: departamentoService,
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+		private _route: ActivatedRoute,
   	) { 
     this.title = "Crea una cuenta";
-    this.user = new User(1,'','','','','','','','','','','');
+    this.loading = false;
 
   }
 
   ngOnInit(): void {
 
-  	console.log("'componente de registro correcto...");
-  	console.log(this._userService.test());
-
-    this.getDepartamentos();
-  }
-
-  getDepartamentos(){
-    
-    this._departamentoService.getDepartamentos().subscribe(
-      response =>{
-        if(response.status == 'success'){
-          this.departamentos = response.departamentos;
-        }
-
-      },
-      error =>{
-        console.log(error);
-
-      });
+    console.log("'componente de registro correcto...");
+    this.buildForm();
 
   }
+
+  
+	private buildForm() {
+
+		this.form = this._formBuilder.group({
+      name: new FormControl('', { validators: [Validators.required, Validators.minLength(3)], updateOn: 'change' }),
+      surname: new FormControl('', { validators: [Validators.required, Validators.minLength(3)], updateOn: 'change' }),
+      email: new FormControl('', { validators: [Validators.required, Validators.email], updateOn: 'change' }),
+      control_number: new FormControl('', { validators: [Validators.required], updateOn: 'change' }),
+      password: new FormControl('', { validators: [Validators.required, Validators.minLength(5)], updateOn: 'change' }),
+      repeat_password: new FormControl('', { validators: [Validators.required, Validators.minLength(5)], updateOn: 'change' })
+		}, {validator: this.checkPasswords});
+
+  }
+
+  checkPasswords(group: FormGroup) { 
+  let pass = group.get('password').value;
+  let confirmPass = group.get('repeat_password').value;
+
+  return pass === confirmPass ? null : { notSame: true }     
+}
+
 
   onSubmit(form){
-  console.log(this.user);
-  this._userService.register(this.user).subscribe(
+  this.loading = true;
+  this._userService.register(form).subscribe(
 	  	response =>{
 	  		if(response.status =="success"){
 	  			this.status = 'success';
-	  			form.reset();
 	  			console.log(response);
 
 	  		}else{
+          this.loading = false;
 	  			this.status = 'error';
 
 	  		} 
 	  		
 	  	},
 	  	error =>{
+        this.loading = false;
 	  		this.status = 'error';
 	  		console.log(<any>error);
 
